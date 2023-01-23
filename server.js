29ea3a6890c96405cbf4171ad9dcca55f2801bde
Marcello Bittencourt <108131718+MarcelloBitt@users.noreply.github.com>
2023-01-23 17:10:22 -0300

    Initial Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    app.emit("pronto");
+  })
+  .catch((e) => console.log(e));
+
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const routes = require("./routes");
+const path = require("path");
+const csrf = require("csurf");
+const { default: helmet } = require("helmet");
+const {
+  middlewareGlobal,
+  checkCrsfError,
+  csrfMiddleware,
+} = require("./src/Middlewares/middleware");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "public")));
+
+const sessionOptions = session({
+  secret: "akascwakkKSuienSHHo27818",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+  },
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+});
+
+app.use(helmet());
+app.use(sessionOptions);
+app.use(flash());
+
+app.set("views", path.resolve(__dirname, "src", "views"));
+app.set("view engine", "ejs");
+
+app.use(csrf());
+// middlewares
+app.use(checkCrsfError);
+app.use(middlewareGlobal);
+app.use(csrfMiddleware);
+app.use(routes);
+
+app.on("pronto", () => {
+  app.listen(3000, () => {
+    console.log("Acessar http://localhost:3000");
+    console.log("Servidor executando na porta 3000");
+  });
+});
